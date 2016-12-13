43a9b0bb6b4e4391af160dbc9b123f38cdbd1608
Zim Kalinowski <zikalino@microsoft.com>
2016-12-13 16:01:45 +0800

    fix for 697635 (#39)
    * fix for 697635
    
    * one more fix
    
    * another fix

--- Lesson3/app/main.c
+++ Lesson3/app/main.c
@@ -39,10 +39,10 @@ static void sendCallback(IOTHUB_CLIENT_CONFIRMATION_RESULT result, void* userCon
     messagePending = false;
 }
 
-static void sendMessageAndBlink(IOTHUB_CLIENT_LL_HANDLE iotHubClientHandle)
+static void sendMessageAndBlink(IOTHUB_CLIENT_LL_HANDLE iotHubClientHandle, char *device_id)
 {
     char buffer[256];
-    snprintf(buffer, sizeof(buffer), "{ deviceId: %s, messageId: %d }", "myraspberrypi", totalBlinkTimes);
+    snprintf(buffer, sizeof(buffer), "{\"deviceId\":\"%s\",\"messageId\":%d}", device_id, totalBlinkTimes);
 
     IOTHUB_MESSAGE_HANDLE messageHandle = IoTHubMessage_CreateFromByteArray(buffer, strlen(buffer));
     if (messageHandle == NULL)
@@ -207,7 +207,7 @@ int main(int argc, char* argv[])
             {
                 if ((lastMessageSentTime + 2000 < millis()) && !messagePending)
                 {
-                    sendMessageAndBlink(iotHubClientHandle);
+                    sendMessageAndBlink(iotHubClientHandle, device_id);
                     totalBlinkTimes++;
                 }
 
